88aaccdf2aa397f6c44ba62193c5adcf7cf47165
Jos van de Wolfshaar <jos.vandewolfshaar@gmail.com>
2018-07-05 00:14:46 +0200

    reformatted the code somewhat

--- dcganmnist/mnist_ssl.py
+++ dcganmnist/mnist_ssl.py
@@ -1,5 +1,3 @@
-import functools
-
 import tensorflow as tf
 import tensorflow.contrib.distributions as tfd
 from official.utils.flags import core as flags_core
@@ -12,58 +10,79 @@ from sklearn.utils import shuffle
 
 
 layers = tf.keras.layers
-models = tf.keras.models
 
 
-def generator_model():
-    model = models.Sequential(name="Generator")
-    conv_kwargs = dict(kernel_size=5, padding='same', activation=None)
+def define_flags():
+    flags_core.define_base()  # Defines batch_size and train_epochs
+    flags_core.define_image()
+    flags_core.set_defaults(
+        batch_size=32, train_epochs=100)
+    flags_core.flags.DEFINE_float(
+        name="lr", default=2e-4,
+        help="Learning rate")
+    flags_core.flags.DEFINE_float(
+        name="stddev", default=1e-2,
+        help="z standard deviation")
+    flags_core.flags.DEFINE_integer(
+        name="num_classes", default=10,
+        help="Number of classes")
+    flags_core.flags.DEFINE_integer(
+        name="z_dim_size", default=100,
+        help="Dimension of noise vector")
+    flags_core.flags.DEFINE_integer(
+        name="num_labeled_examples", default=400,
+        help="Number of labeled examples per class")
+    flags_core.flags.DEFINE_bool(
+        name="man_reg", default=False,
+        help="Manifold regularization")
 
-    def conv2d_block(filters, strides, transpose=True, activation=tf.nn.relu,
-                         suffix="Generator", index=0):
-        suffix = suffix + str(index)
-        if transpose:
-            model.add(layers.UpSampling2D(name="Upsampling" + suffix))
-            model.add(layers.Conv2D(
-                filters=filters, **conv_kwargs, name="Conv2DTranspose" + suffix))
-        else:
-            model.add(layers.Conv2D(filters=filters, strides=strides, **conv_kwargs,
-                                    name="Conv2D" + suffix))
-        model.add(layers.Activation(activation=activation, name="Activation" + suffix))
 
-    model.add(layers.Dense(7 * 7 * 64, activation=None, name="NoiseToSpatial"))
-    model.add(layers.Activation(tf.nn.relu, name="NoiseToSpatialActivation"))
+def define_generator():
+
+    def conv2d_block(filters, upsample=True, activation=tf.nn.relu, index=0):
+        if upsample:
+            model.add(layers.UpSampling2D(name="UpSampling" + str(index), size=(2, 2)))
+        model.add(layers.Conv2D(
+            filters=filters, kernel_size=5, padding='same', name="Conv2D" + str(index),
+            activation=activation))
+
+    # From flat noise to spatial
+    model = tf.keras.models.Sequential(name="Generator")
+    model.add(layers.Dense(7 * 7 * 64, activation=tf.nn.relu, name="NoiseToSpatial"))
     model.add(layers.Reshape((7, 7, 64)))
 
-    conv2d_block(filters=128, strides=2, transpose=True, index=0)
-    conv2d_block(filters=64, strides=2, transpose=True, index=1)
-    conv2d_block(filters=64, strides=1, transpose=False, index=2)
-    conv2d_block(filters=1, strides=1, transpose=False, activation=tf.nn.tanh, index=3)
+    # Four blocks of convolutions, 2 that upsample and convolve, and 2 more that
+    # just convolve
+    conv2d_block(filters=128, upsample=True, index=0)
+    conv2d_block(filters=64, upsample=True, index=1)
+    conv2d_block(filters=64, upsample=False, index=2)
+    conv2d_block(filters=1, upsample=False, activation=tf.nn.tanh, index=3)
     return model
 
 
 class Discriminator:
 
     def __init__(self):
+        """The discriminator network. Split up in a 'tail' and 'head' network, so that we can
+        easily get the """
         self.tail = self._define_tail()
         self.head = self._define_head()
 
     def _define_tail(self, name="Discriminator"):
-        conv_kwargs = dict(kernel_size=5, padding='same', activation=None)
-        feature_model = models.Sequential(name=name)
+        """Defines the network until the intermediate layer that can be used for feature-matching
+        loss."""
+        feature_model = tf.keras.models.Sequential(name=name)
 
-        # Leaky ReLU with alpa=0.2
-        activation_fn = functools.partial(tf.nn.leaky_relu, alpha=0.2)
-
-        def conv2d_dropout(filters, strides, activation=activation_fn, index=0):
+        def conv2d_dropout(filters, strides, index=0):
+            # Adds a convolution followed by a Dropout layer
             suffix = str(index)
             feature_model.add(layers.Conv2D(
-                filters=filters, strides=strides, **conv_kwargs, name="Conv{}".format(suffix)))
-            feature_model.add(layers.Activation(
-                activation=activation, name="Activation{}".format(suffix)))
+                filters=filters, strides=strides, name="Conv{}".format(suffix), padding='same',
+                kernel_size=5, activation=tf.nn.leaky_relu))
             feature_model.add(layers.Dropout(name="Dropout{}".format(suffix), rate=0.3))
 
-        # Three blocks of convs and dropouts
+        # Three blocks of convs and dropouts. They all have 5x5 kernels, leaky ReLU and 0.3
+        # dropout rate.
         conv2d_dropout(filters=32, strides=2, index=0)
         conv2d_dropout(filters=64, strides=2, index=1)
         conv2d_dropout(filters=64, strides=1, index=2)
@@ -73,57 +92,56 @@ class Discriminator:
         return feature_model
 
     def _define_head(self):
-        head_model = models.Sequential(name="DiscriminatorHead")
+        # Defines the remaining layers after the 'tail'
+        head_model = tf.keras.models.Sequential(name="DiscriminatorHead")
         head_model.add(layers.Dense(units=10, activation=None, name="Logits"))
         return head_model
 
     @property
     def trainable_variables(self):
+        # Return both tail's parameters a well as those of the head
         return self.tail.trainable_variables + self.head.trainable_variables
 
     def __call__(self, x, *args, **kwargs):
+        # By adding this, the code below can treat a Discriminator instance as a
+        # tf.keras.models.Sequential instance
         features = self.tail(x, *args, **kwargs)
         return self.head(features, *args, **kwargs), features
 
 
-def define_flags():
-    flags_core.define_base()  # Defines batch_size and train_epochs
-    flags_core.define_image()
-    flags_core.set_defaults(batch_size=32, train_epochs=15)
-    flags_core.flags.DEFINE_float(name="lr", default=2e-4, help="Learning rate")
-    flags_core.flags.DEFINE_float(name="stddev", default=1e-2, help="z(x) standard deviation")
-    flags_core.flags.DEFINE_integer(name="num_classes", default=10, help="Number of classes")
-    flags_core.flags.DEFINE_integer(name="z_dim_size", default=100,
-                                    help="Dimension of noise vector")
-    flags_core.flags.DEFINE_integer(name="num_labeled_examples", default=400,
-                                    help="Number of labeled examples per class")
+def accuracy(logits, labels):
+    """Compute accuracy for this mini-batch """
+    preds = tf.argmax(logits, axis=1)
+    return tf.reduce_mean(tf.to_float(tf.equal(preds, labels)))
 
-def weight_normalize(layer):
-    layer.V = layer.add_weight()
 
 def main(_):
     flags_obj = flags.FLAGS
     with tf.Graph().as_default():
+        # Setup th einput pipeline
         (images_lab, labels_lab), (images_unl, labels_unl), (images_unl2, labels_unl2), \
-        (images_test, labels_test) = prepare_input_pipeline(flags_obj)
+            (images_test, labels_test) = prepare_input_pipeline(flags_obj)
 
-        # Setup noise vector
-        with tf.name_scope("LatentNoiseVector"):
-            z = tfd.Normal(loc=0.0, scale=flags_obj.stddev).sample(
-                sample_shape=(tf.shape(images_lab)[0], flags_obj.z_dim_size))
+        with tf.name_scope("BatchSize"):
+            batch_size_tensor = tf.shape(images_lab)[0]
+
+        # Get the noise vectors
+        z, z_perturbed = define_noise(batch_size_tensor, flags_obj)
 
         # Generate images from noise vector
         with tf.name_scope("Generator"):
-            g_model = generator_model()
-            generated_images = g_model(z)
+            g_model = define_generator()
+            images_fake = g_model(z)
+            images_fake_perturbed = g_model(z_perturbed)
 
         # Discriminate between real and fake, and try to classify the labeled data
         with tf.name_scope("Discriminator") as discriminator_scope:
             d_model = Discriminator()
-            logits_fake, features_fake = d_model(generated_images, training=True)
-            logits_real_unl, features_real_unl = d_model(images_unl, training=True)
-            logits_real_lab, features_real_lab = d_model(images_lab, training=True)
-            logits_train, _ = d_model(images_lab, training=False)
+            logits_fake, features_fake          = d_model(images_fake, training=True)
+            logits_fake_perturbed, _            = d_model(images_fake_perturbed, training=True)
+            logits_real_unl, features_real_unl  = d_model(images_unl, training=True)
+            logits_real_lab, features_real_lab  = d_model(images_lab, training=True)
+            logits_train, _                     = d_model(images_lab, training=False)
 
         # Set the discriminator losses
         with tf.name_scope("DiscriminatorLoss"):
@@ -139,21 +157,20 @@ def main(_):
                 tf.reduce_mean(tf.nn.softplus(logits_sum_real)) +
                 tf.reduce_mean(tf.nn.softplus(logits_sum_fake)))
             loss_d = loss_supervised + loss_unsupervised
-
-        def accuracy(logits, labels):
-            preds = tf.argmax(logits, axis=1)
-            return tf.reduce_mean(tf.to_float(tf.equal(preds, labels)))
+            if flags_obj.man_reg:
+                loss_d += 1e-3 * tf.nn.l2_loss(logits_fake - logits_fake_perturbed) \
+                    / tf.to_float(batch_size_tensor)
 
         # Configure discriminator training ops
         with tf.name_scope("Train") as train_scope:
-            optimizer = tf.train.GradientDescentOptimizer(flags_obj.lr * 0.1)
+            optimizer = tf.train.AdamOptimizer(flags_obj.lr * 0.25)
             optimize_d = optimizer.minimize(loss_d, var_list=d_model.trainable_variables)
             train_accuracy_op = accuracy(logits_train, labels_lab)
 
         with tf.name_scope(discriminator_scope):
             with tf.control_dependencies([optimize_d]):
                 # Build a second time, so that new variables are used
-                logits_fake, features_fake = d_model(generated_images, training=True)
+                logits_fake, features_fake = d_model(images_fake, training=True)
                 logits_real_unl, features_real_unl = d_model(images_unl2, training=True)
 
         # Set the generator loss and the actual train op
@@ -177,7 +194,7 @@ def main(_):
             summary_op = tf.summary.merge([
                 tf.summary.scalar("LossDiscriminator", loss_d),
                 tf.summary.scalar("LossGenerator", loss_g),
-                tf.summary.image("GeneratedImages", generated_images),
+                tf.summary.image("GeneratedImages", images_fake),
                 tf.summary.scalar("ClassificationAccuracyTrain", train_accuracy_op),
                 tf.summary.scalar("ClassificationAccuracyTest", test_accuracy_op)])
         writer = tf.summary.FileWriter(_next_logdir("tensorboard/mnist_ssl"))
@@ -194,7 +211,7 @@ def main(_):
                 print("Epoch {}".format(epoch))
                 pbar = tqdm.trange(steps_per_epoch)
                 for _ in pbar:
-                    if step % 100 == 0:
+                    if step % 1000 == 0:
                         # Look Ma, no feed_dict!
                         _, loss_g_batch, loss_d_batch, summ, accuracy_batch = sess.run(
                             [train_op, loss_g, loss_d, summary_op, train_accuracy_op])  
@@ -215,7 +232,17 @@ def main(_):
 
                 # Classify test data
                 accuracies = [sess.run(test_accuracy_op) for _ in range(steps_per_test)]
-                print("Test accuracy: {0:.4f}", np.mean(accuracies))
+                print("Test accuracy: {0:.4f}".format(np.mean(accuracies)))
+
+
+def define_noise(batch_size_tensor, flags_obj):
+    # Setup noise vector
+    with tf.name_scope("LatentNoiseVector"):
+        z = tfd.Normal(loc=0.0, scale=flags_obj.stddev).sample(
+            sample_shape=(batch_size_tensor, flags_obj.z_dim_size))
+        z_perturbed = z + tfd.Normal(loc=0.0, scale=flags_obj.stddev).sample(
+            sample_shape=(batch_size_tensor, flags_obj.z_dim_size)) * 1e-5
+    return z, z_perturbed
 
 
 def prepare_input_pipeline(flags_obj):
@@ -263,6 +290,8 @@ def prepare_input_pipeline(flags_obj):
             (train_x_unlabeled, train_y_unlabeled.astype(np.int64)), len(train_x_labeled))
         images_unl, labels_unl = train_ds_unl.get_next()
 
+        # Setup another pipeline that also uses the unlabeled data, so that we use a different
+        # batch for computing the discriminator loss and the generator loss
         train_x_unlabeled, train_y_unlabeled = shuffle(train_x_unlabeled, train_y_unlabeled)
         train_ds_unl2 = train_pipeline(
             (train_x_unlabeled, train_y_unlabeled.astype(np.int64)), len(train_x_labeled))
